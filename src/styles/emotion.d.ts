b3477dc4821a53097c42be02db9215b31989cfe7
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-14 23:28:46 +0900

    feat: emotion.d.ts

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,60 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface DefaultTheme {
+    colors: {
+      orange000: string;
+      orange100: string;
+      orange200: string;
+      orange300: string;
+      orange400: string;
+      orange500: string;
+      gray100: string;
+      gray200: string;
+      gray300: string;
+      gray400: string;
+      white: string;
+      white200: string;
+      white300: string;
+      white400: string;
+      white500: string;
+      red100: string;
+    };
+    fonts: {
+      h1: string;
+      h2: string;
+      h3: string;
+      h4: string;
+      h5: string;
+      h6: string;
+      Body1: string;
+      Body2: string;
+      Body3: string;
+      Caption1: string;
+      Caption2: string;
+      header: string;
+      header00: string;
+      header001: string;
+      header0: string;
+      header1: string;
+      header2: string;
+      header3: string;
+      header4: string;
+      body00: string;
+      body0: string;
+      body1: string;
+      body2: string;
+      body3: string;
+      body35: string;
+      body4: string;
+      body5: string;
+      body6: string;
+      caption: string;
+      caption2: string;
+      button: string;
+      button2: string;
+      button3: string;
+      button4: string;
+    };
+  }
+}
