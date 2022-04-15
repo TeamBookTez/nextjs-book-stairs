b98c16e54c5d11820284166202d176039983d82e
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-15 12:44:10 +0900

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 
-import { ThemeProvider } from "@emotion/react";
+import { Global, ThemeProvider } from "@emotion/react";
 import type { AppProps } from "next/app";
 
+import GlobalStyle, { resetStyle } from "../styles/globalStyle";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={resetStyle} />
+      <Global styles={GlobalStyle} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
