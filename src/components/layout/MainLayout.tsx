45ef55236ea7ea7aced211d793cd6f4be6fac243
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-15 12:16:41 +0900

    refactor: MainLayout file theme colors

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -24,7 +24,7 @@ export default function Layout(props: LayoutProps) {
 }
 
 const StLayoutWrapper = styled.section`
-  background-color: #242424;
+  background-color: ${({ theme }) => theme.colors.gray100};
 `;
 
 const StWrapper = styled.main`
@@ -35,6 +35,6 @@ const StWrapper = styled.main`
   margin-left: 17.5rem; //NAV_WRAPPER_WIDTH
 
   border-radius: 2rem 0 0 2rem;
-  border: 0.1rem solid #ffffff;
-  background-color: #ffffff;
+  border: 0.1rem solid ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.white};
 `;
