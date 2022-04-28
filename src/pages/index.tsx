3c1d093d60c5db4a2dac4dca43155c7fbe3506d8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-04-28 16:56:07 +0900

    feat: add useMediaQuery for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,89 @@
+import styled from "@emotion/styled";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
 
-import styles from "../styles/Home.module.css";
+import { Loading } from "../components/common";
+import {
+  LandingFive,
+  LandingFooter,
+  LandingFour,
+  LandingHeader,
+  LandingOne,
+  LandingThree,
+  LandingTwo,
+  MobileLandingFive,
+  MobileLandingFooter,
+  MobileLandingFour,
+  MobileLandingHeader,
+  MobileLandingOne,
+  MobileLandingThree,
+  MobileLandingTwo,
+} from "../components/landing";
+import { isLoginState } from "../core/atom";
+import useCheckLoginState from "../util/hooks/useCheckLoginState";
+import { Desktop, Mobile } from "../util/hooks/useMediaQuery";
 
 const Landing: NextPage = () => {
+  const router = useRouter();
+  const { isLogin, isLoginLoading } = useCheckLoginState();
+  const setIsLogin = useSetRecoilState(isLoginState);
+
+  useEffect(() => {
+    if (isLogin) {
+      setIsLogin(true);
+      router.push("/main");
+    } else {
+      setIsLogin(false);
+    }
+  }, [isLogin]);
+
   return (
-    <div className={styles.container}>
-      <div>pages/index.tsx 여기가 랜딩 페이지가 되면 되지 않을까?</div>
-    </div>
+    <>
+      {isLoginLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Mobile>
+            <MobileLandingHeader />
+            <MobileLandingOne />
+            <MobileLandingTwo />
+            <MobileLandingThree />
+            <MobileLandingFour />
+            <MobileLandingFive />
+            <MobileLandingFooter />
+          </Mobile>
+          <Desktop>
+            <StLandingWrapper>
+              <LandingHeader />
+              <StMain>
+                <LandingOne />
+                <LandingTwo />
+                <LandingThree />
+                <LandingFour />
+                <LandingFive />
+              </StMain>
+            </StLandingWrapper>
+            <LandingFooter />
+          </Desktop>
+        </>
+      )}
+    </>
   );
 };
+
+const StMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 0 11.5rem;
+`;
+
+const StLandingWrapper = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+`;
 
 export default Landing;
