f3ec2e1a51e24ec90c998ecc21496c25dbdaa925
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-02 23:50:40 +0900

    feat: modify next.config.fs for use of svg component

--- public/assets/asset.d.ts
+++ public/assets/asset.d.ts
@@ -3,9 +3,9 @@ declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
 
-declare module "*.svg" {
-  import React = require("react");
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
+// declare module "*.svg" {
+//   import React = require("react");
+//   export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+//   const src: string;
+//   export default src;
+// }
