29b7b994c7fbbc785d99cee00213e1d0f790ca9b
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-03 01:16:34 +0900

    refactor: separate my page information links

--- src/components/myPage/ServiceContent.tsx
+++ src/components/myPage/ServiceContent.tsx
@@ -6,6 +6,7 @@ import { useRecoilValue } from "recoil";
 import { ImgReadNum } from "../../../public/assets/images";
 import { isLoginState } from "../../core/atom";
 import { UserInfo } from "../../types/myPage";
+import myPageInformations from "../../util/myPageInformations";
 import { BookComment } from ".";
 
 interface ServiceContentProps {
@@ -32,42 +33,14 @@ export default function ServiceContent(props: ServiceContentProps) {
         </StCountBook>
       )}
       <StServiceWrapper>
-        <StServiceInfo>
-          <a
-            href="https://rose-prepared-583.notion.site/6e6807cf2fff4effbd108057e611d5b9"
-            target="_blank"
-            rel="noopener noreferrer">
-            <StInfoTitle>개인정보 처리 방침</StInfoTitle>
-            <StInfoDesc>북스테어즈는 개인정보보호 규정, 가이드라인을 준수하고 있어요.</StInfoDesc>
-          </a>
-        </StServiceInfo>
-        <StServiceInfo>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSd91WwzYqTpCzfMFOkrbwUtFiMGK7YGkP36lWbmsgPFomvNUg/viewform"
-            target="_blank"
-            rel="noopener noreferrer">
-            <StInfoTitle>서비스 문의</StInfoTitle>
-            <StInfoDesc>북스테어즈를 이용하며 문의하고 싶은 점들을 남겨주세요</StInfoDesc>
-          </a>
-        </StServiceInfo>
-        <StServiceInfo>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdlnQz0Nwx8vtqmHtgNEkzPwUy-LsTMjt9KXlmsAGZiu1FJRw/viewform"
-            target="_blank"
-            rel="noopener noreferrer">
-            <StInfoTitle>피드백 남기기</StInfoTitle>
-            <StInfoDesc>북스테어즈를 사용하시면서 느낀 점들에 대해 남겨주세요</StInfoDesc>
-          </a>
-        </StServiceInfo>
-        <StServiceInfo>
-          <a
-            href="https://rose-prepared-583.notion.site/b644acc67642448584a2cc6528dcbd7d"
-            target="_blank"
-            rel="noopener noreferrer">
-            <StInfoTitle>함께하는 사람들</StInfoTitle>
-            <StInfoDesc>북스테어즈를 만들어가는 팀원들을 소개할게요.</StInfoDesc>
-          </a>
-        </StServiceInfo>
+        {myPageInformations.map((info) => (
+          <StServiceInfo key={info.title}>
+            <a href={info.href} target="_blank" rel="noopner noreferrer">
+              <StInfoTitle>{info.title}</StInfoTitle>
+              <StInfoDesc>{info.description}</StInfoDesc>
+            </a>
+          </StServiceInfo>
+        ))}
       </StServiceWrapper>
       {children}
     </StWrapper>
