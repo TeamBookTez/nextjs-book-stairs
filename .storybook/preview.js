03f4d39e0a5fa071f88d5b1f2c87242eb4f17bc5
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-13 16:32:35 +0900

    init: set storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { Global, ThemeProvider } from "@emotion/react";
+import GlobalStyle, { resetStyle } from "../src/styles/globalStyle";
+import theme from "../src/styles/theme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,14 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story, context) => (
+    <ThemeProvider theme={theme}>
+      <Global styles={resetStyle} />
+      <Global styles={GlobalStyle} />
+      <Story {...context} />
+    </ThemeProvider>
+  ),
+];
