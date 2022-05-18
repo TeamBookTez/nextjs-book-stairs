6b1cb7a479076cb200500fc0663a93830637fc75
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-18 21:09:07 +0900

    [feat] storybook :: emotion setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
+const path = require("path");
+const resolvePath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: "@storybook/react",
+  // framework: "@storybook/react",
   // core: {
   //   builder: "@storybook/builder-webpack5",
   // },
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        "@emotion/core": resolvePath("node_modules/@emotion/react"),
+        "@emotion/styled": resolvePath("node_modules/@emotion/styled"),
+        "emotion-theming": resolvePath("node_modules/@emotion/react"),
+      },
+    },
+  }),
 };
---
+++ src/stories/LandingHeader.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, Story } from "@storybook/react";
+
+import { LandingHeader } from "../components/landing";
+
+// 스토리북의 폴더
+export default {
+  title: "Landing/header",
+  component: LandingHeader,
+} as Meta;
+
+//-----------------------
+
+// Story Template 정의
+const Template: Story = () => {
+  return <LandingHeader />;
+};
+
+// Story1 정의
+export const MyComponentStory1 = Template.bind({});
+// MyComponentStory1.args = {
+//   name: "",
+//   phoneNumber: "",
+//   distance: "",
+//   address: "",
+// };
+
+// MyComponentStory1.decorators = [(Story) => <div style={{ margin: '3em' }}><Story/></div>];
+
+// MyComponentStory1.parameters = {
+//   backgrounds: {
+//     values: [
+//       { name: "lightBlue", value: "lightBlue" },
+//       { name: "gray", value: "#eeeeee" },
+//     ],
+//   },
+// };
+
+// MyComponentStory1.storyName = "Custom";
