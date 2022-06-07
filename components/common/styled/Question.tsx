fa135b2fda8099181bb6cdf7b23505dfcfdbd527
SNUPI <joohaem@daum.net>
2022-06-07 17:19:22 +0900

    [feat] add common styled modules

--- components/common/styled/Question.tsx
+++ components/common/styled/Question.tsx
@@ -1,3 +1,13 @@
+/*
+마지막 편집자: 22-06-07 joohaem
+변경사항 및 참고:
+  - "StLabelQuestion" 은 한 곳(components/common/LabelQuestion)에서밖에 쓰이지않아
+    해당 파일에서 선언해주었으니, 따로 선언해주지 않아도 됩니다
+    
+고민점:
+  - 
+*/
+
 export default function Question() {
   return <div>Question</div>;
 }
