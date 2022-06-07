9aa3569fc36f9ca6f40bd927c5c61eeff3ba5d45
SNUPI <joohaem@daum.net>
2022-06-07 17:55:42 +0900

    [etc] 미사용 모듈 제거

--- components/bookNote/preNote/PreNoteFormContainer.tsx
+++ components/bookNote/preNote/PreNoteFormContainer.tsx
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 import React from "react";
 
-import { DrawerIdx } from "../../../pages/book-note/[reviewId]";
 import ExampleDrawerBtn from "../ExampleDrawerBtn";
 import StepUpBtn from "../StepUpBtn";
 
