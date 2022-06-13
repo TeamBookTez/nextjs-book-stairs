1f9c9de36a2c8dfca414c70f2da7f07e6e264ec5
soryeongk <soryeongk.kr@gmail.com>
2022-06-14 02:02:31 +0900

    feat: component 옮기기

--- components/common/AlertLabel.tsx
+++ components/common/AlertLabel.tsx
@@ -1,3 +1,43 @@
-export default function AlertLabel() {
-  return <div>AlertLabel</div>;
+import styled from "@emotion/styled";
+
+import { IcAlert } from "../../public/assets/icons";
+
+interface AlertLabelProps {
+  message: string | undefined;
 }
+
+export default function AlertLabel(props: AlertLabelProps) {
+  const { message } = props;
+
+  return (
+    <StLabelWrapper>
+      <StAlert />
+      <StStrong>{message}</StStrong>
+    </StLabelWrapper>
+  );
+}
+
+const StLabelWrapper = styled.p`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 2.4rem;
+  margin: 1.3rem 0 0 0.4rem;
+`;
+
+const StAlert = styled(IcAlert)`
+  height: 100%;
+  width: auto;
+  margin-right: 0.4rem;
+`;
+
+const StStrong = styled.strong`
+  display: flex;
+  align-items: center;
+  /* 임의 폰트 */
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 1.6rem;
+  color: ${({ theme }) => theme.colors.red100};
+`;
--- components/common/styled/Input.tsx
+++ components/common/styled/Input.tsx
@@ -1,3 +1,19 @@
-export default function Input() {
-  return <div>Input</div>;
-}
+import styled from "@emotion/styled";
+
+export const Input = styled.input`
+  width: 100%;
+  height: 5.4rem;
+  padding-left: 2rem;
+
+  background-color: ${({ theme }) => theme.colors.white200};
+
+  border: 0.2rem solid ${({ theme }) => theme.colors.white200};
+  border-radius: 1rem;
+
+  ${({ theme }) => theme.fonts.body3}
+  color: ${({ theme }) => theme.colors.gray100};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray400};
+  }
+`;
