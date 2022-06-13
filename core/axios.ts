3750dc49c28271865d146e5a417ef0fecdeae8db
soryeongk <soryeongk@yolocorp.kr>
2022-06-13 18:15:42 +0900

    feat: axios setting (on process)

--- core/axios.ts
+++ core/axios.ts
@@ -1,4 +1,32 @@
 import axios from "axios";
+import LocalStorage from "./localStorage";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+
+const getAccessToken = LocalStorage.getItem("booktez-token");
+
+export interface Response<T> {
+  data: T;
+  message: string;
+  status: number;
+  success: boolean;
+}
+
+export const baseInstance = 
+  axios.create({
+  baseURL: `${BASE_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "",
+  }});
+
+export const kakaoInstance = axios.create({
+  baseURL: "https://dapi.kakao.com",
+  headers: {
+    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+  },
+});
 
 export const KAKAO = axios.create({
   baseURL: "https://dapi.kakao.com",
