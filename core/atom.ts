c3208c1819a2aeb422f992bded8522727ff0be4e
SNUPI <joohaem@daum.net>
2022-06-15 21:50:58 +0900

    [etc] 북노트 url 이름 변경

--- core/atom.ts
+++ core/atom.ts
@@ -12,8 +12,8 @@ export const isLoginState = atom<boolean>({
 export const navigatingBookInfoState = atom<NavigatingBookInfoState>({
   key: "navigatingBookInfoState",
   default: {
-    reviewId: "-1",
-    title: "들꽃의 노래",
+    reviewId: "62a9c46d1b74916b7a94dcb3",
+    title: "북노오오오오오트으으으으",
     fromUrl: "",
     fromSt: "/main",
   },
