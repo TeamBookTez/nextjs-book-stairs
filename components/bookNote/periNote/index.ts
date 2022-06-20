135b06814a1a9495bd15510162769e0696af0b57
SNUPI <joohaem@daum.net>
2022-06-20 17:38:08 +0900

    etc: indexing

--- components/bookNote/periNote/index.ts
+++ components/bookNote/periNote/index.ts
@@ -1,2 +1,5 @@
+export { default as ChildQANode } from "./ChildQANode";
 export { default as HeaderLabel } from "./HeaderLabel";
 export { default as PeriNote } from "./PeriNote";
+export { default as TopAnswerContainer } from "./TopAnswerContainer";
+export { default as TopQuestionContainer } from "./TopQuestionContainer";
