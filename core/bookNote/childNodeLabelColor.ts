fcc47b2eefbcf0cd0b3109735a6faa823712ab51
SNUPI <joohaem@daum.net>
2022-06-21 21:20:32 +0900

    feat: add labelColorList on core folder

---
+++ core/bookNote/childNodeLabelColor.ts
@@ -0,0 +1,12 @@
+export const labelColorList = [
+  "#FF3D00", // theme.colors.orange000
+  "#FF3D00", // theme.colors.orange000
+  "#FF4C00", // theme.colors.orange100
+  "#FF4C00", // theme.colors.orange100
+  "#FF7F49", // theme.colors.orange300
+  "#FF7F49", // theme.colors.orange300
+  "#FFAD8A", // theme.colors.orange400
+  "#FFAD8A", // theme.colors.orange400
+  "#FFE9E0", // theme.colors.orange500
+  "#FFE9E0", // theme.colors.orange500
+];
