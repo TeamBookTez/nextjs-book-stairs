51367ea9fd2455154d4061f3f750ba57a9d544d2
soryeongk <soryeongk.kr@gmail.com>
2022-07-03 17:24:55 +0900

    fix: 북노트 나가기 button과 Link 충돌

--- components/bookNote/ExitModal.tsx
+++ components/bookNote/ExitModal.tsx
@@ -16,15 +16,7 @@ import { useRecoilValue } from "recoil";
 import { navigatingBookInfoState } from "../../core/atom";
 import { ImgExit } from "../../public/assets/images";
 import { ImageWrapper } from "../common/styled/Img";
-import {
-  StBtnCancel,
-  StBtnDelete,
-  StBtnWrapper,
-  StDetail,
-  StPopUp,
-  StPopUpWrapper,
-  StQuestion,
-} from "../common/styled/PopUp";
+import { StBtnCancel, StBtnWrapper, StDetail, StPopUp, StPopUpWrapper, StQuestion } from "../common/styled/PopUp";
 
 interface ExitModalProps {
   onClickCancelBtn: () => void;
@@ -46,16 +38,14 @@ export default function ExitModal(props: ExitModalProps) {
           <StBtnCancel type="button" onClick={onClickCancelBtn}>
             취소
           </StBtnCancel>
-          <StBtnDelete type="button">
-            <StLink
-              href={{
-                pathname: fromUrl,
-                query: { reviewId },
-              }}
-              passHref>
-              나가기
-            </StLink>
-          </StBtnDelete>
+          <Link
+            href={{
+              pathname: fromUrl,
+              query: { reviewId },
+            }}
+            passHref>
+            <StLink>나가기</StLink>
+          </Link>
         </StBtnWrapper>
       </StPopUp>
     </StPopUpWrapper>
@@ -69,11 +59,21 @@ const StImg = styled(ImageWrapper)`
   height: 12.6rem;
 `;
 
-const StLink = styled(Link)`
+const StLink = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  width: 100%;
-  height: 100%;
+  border-radius: 0.8rem;
+
+  ${({ theme }) => theme.fonts.button}
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.orange100};
+
+  color: ${({ theme }) => theme.colors.white};
+
+  width: 8rem;
+  height: 4.6rem;
+
+  cursor: pointer;
 `;
