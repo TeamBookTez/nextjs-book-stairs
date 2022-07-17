8ce8a1bb0d3ecf50f574a1e49894de539fdc5b6c
soryeongk <soryeong.kim@autopediacar.com>
2022-07-17 14:56:28 +0900

    fix: replace static images file to s3 bucket

--- components/myPage/ServiceContent.tsx
+++ components/myPage/ServiceContent.tsx
@@ -21,7 +21,11 @@ export default function ServiceContent(props: ServiceContentProps) {
       {isLogin && userInfo && (
         <StCountBook>
           <StImgWrapper>
-            <Image src="/assets/images/mypageReadImage.png" alt="읽은 책 수" layout="fill" />
+            <Image
+              src="https://bookstairs-bucket.s3.ap-northeast-2.amazonaws.com/mypageReadImage.png"
+              alt="읽은 책 수"
+              layout="fill"
+            />
           </StImgWrapper>
           <StContentWrapper>
             <BookComment reviewCount={userInfo.reviewCount} />
