0fcf983a9dffbc386d9b15d36e369a6e44dc42d5
soryeongk <soryeong.kim@autopediacar.com>
2022-07-23 15:58:40 +0900

    fix: replace <img /> to <Image />

--- components/signup/LastStep.tsx
+++ components/signup/LastStep.tsx
@@ -3,18 +3,20 @@
 변경사항 및 참고:
   - react router dom을 사용하지 않는 넥제를 대비해 Outlet을 사용하지 않는 방향으로 바꾸었습니다.
   - 이에 따라 Router.tsx 파일도 변경되었습니다!
-    
+
 고민점:
   - 별도의 타입 가드없이 nickname이 string | null로 되도록 해두었는데,
     Next.js로 옮길 때 nickname이 null인 경우에는 메인으로 리디렉션하는 것 넣으면 좋을 듯합니다.
 */
 import styled from "@emotion/styled";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { baseInstance } from "../../core/axios";
 import LocalStorage from "../../core/localStorage";
 import { ImgSignUpFinish } from "../../public/assets/images";
+import { imageLoader } from "../../util/imageLoader";
 import { Loading } from "../common";
 import { DefaultButton } from "../common/styled/Button";
 import { StSignupHeading2 } from "../common/styled/Signup";
@@ -69,7 +71,13 @@ export default function LastStep() {
             나만의 서재가 완성됐어요!
           </StSignupHeading2>
 
-          <img src={ImgSignUpFinish.src} alt="회원 가입 완료시 뜨는 그래픽 문구입니다" />
+          <Image
+            src={ImgSignUpFinish.src}
+            alt="회원 가입 완료시 뜨는 그래픽 문구입니다"
+            loader={imageLoader}
+            width={464}
+            height={213}
+          />
           <StHomeBtn type="button" onClick={goNextStep}>
             홈 바로가기
           </StHomeBtn>
