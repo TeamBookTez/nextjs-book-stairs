f5993df037401bc3c0a0366a0ee084a4a40d99a1
SNUPI <joohaem@daum.net>
2022-12-22 12:36:27 +0900

    fix: saveStatelessPeriNoteData (useForm -> setData) 시점 분리

--- components/bookNote/periNote/PeriNote.tsx
+++ components/bookNote/periNote/PeriNote.tsx
@@ -1,14 +1,15 @@
 /*
 마지막 편집자: 22-06-18 joohaem
 변경사항 및 참고:
-  - TopQuestionContainer 안에 TopAnswerContainer 안에 ChildQANode
-
   - toggleMenu 살펴보기 (더보기 메뉴 DOM 접근?) / StMoreIcon (.icn_more) - StMenuWrapper (.isPriQ)
   - deepCopyTree --> immer.js 로 변경
 
 고민점:
   - 로그인 loading -> initial data 표시 -> fetching loading 단계로 로딩이 이루어지는데, 통합이 필요할 것 같습니다,!
     => 기획에서 별 얘기 없으면 해결된 것
+  
+    - useFetchBookNote 같은 함수들의 pre/peri 의 분리가 필요함 (타입지정 등을 명확히 함으로써 이후의 유지/보수가 원활해질 것)
+    - saveStatelessPeriNoteData() 함수 호출의 정의를 명확히 해야 함 (useForm과 useFetchBookNote 데이터의 일치 시점)
 */
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
@@ -68,7 +69,8 @@ export default function PeriNote(props: PeriNoteProps) {
   const handleAddChild = (path: number[], currentIndex?: number) => {
     // currentIndex가 있으면 "answer", 없으면 "question" 추가
     const isAddAnswer = currentIndex !== undefined;
-    const newRoot = isAddAnswer ? saveStatelessPeriNoteData() : deepCopyTree(data.answerThree);
+    // FIX :: const newRoot = isAddAnswer ? saveStatelessPeriNoteData() : deepCopyTree(data.answerThree);
+    const newRoot = deepCopyTree(data.answerThree);
     const current = getNodeByPath(newRoot, path);
 
     if (isAddAnswer) {
