dc57c7f8da4a9763953fddc33c8c8486361386b2
SNUPI <joohaem@daum.net>
2022-12-30 15:38:10 +0900

    refactor: state enum 분리

--- core/atom/index.ts
+++ core/atom/index.ts
@@ -4,13 +4,18 @@ import { recoilPersist } from "recoil-persist";
 import { NavigatingBookInfoState } from "../../types/bookcase";
 const { persistAtom } = recoilPersist();
 
+enum StateType {
+  IS_LOGIN_STATE = "isLoginState",
+  NAVIGATING_BOOK_INFO_STATE = "navigatingBookInfoState",
+}
+
 export const isLoginState = atom<boolean>({
-  key: "isLoginState",
+  key: StateType.IS_LOGIN_STATE,
   default: false,
 });
 
 export const navigatingBookInfoState = atom<NavigatingBookInfoState>({
-  key: "navigatingBookInfoState",
+  key: StateType.NAVIGATING_BOOK_INFO_STATE,
   default: {
     reviewId: "62b5581c1b74916b7a94efc8",
     title: "북노오오오오오트으으으으",
