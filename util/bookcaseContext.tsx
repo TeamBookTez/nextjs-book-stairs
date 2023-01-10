dc087eaf87a6608931c169f51754aa7a4279214e
soryeongk <soryeongk.kr@gmail.com>
2023-01-11 01:51:54 +0900

    feat: 서재 네비게이션 context provider 생성

---
+++ util/bookcaseContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, ReactNode, useMemo, useState } from "react";
+import { useRecoilValue } from "recoil";
+
+import { navigatingBookInfoState } from "../core/atom";
+import { BookcasePathKey, bookcasePathKey } from "../types/bookcase";
+
+type BookcaseNavigationContextType = {
+  navIndex: { key: BookcasePathKey; value: number };
+  changeNavIndex?: (idx: BookcasePathKey) => void;
+};
+
+export const BookcaseNavigationContext = createContext<BookcaseNavigationContextType>({
+  navIndex: { key: bookcasePathKey.all, value: 0 },
+});
+
+export const BookcaseNavigationProvider = ({ children }: { children: ReactNode }) => {
+  const navigatingBookInfo = useRecoilValue(navigatingBookInfoState);
+  const { fromSt } = navigatingBookInfo;
+  const [navIndex, setNavIndex] = useState<BookcasePathKey>(fromSt);
+
+  const changeNavIndex = (idx: BookcasePathKey) => {
+    setNavIndex(idx);
+  };
+
+  const navUnderbarIndex = useMemo(() => {
+    switch (navIndex) {
+      case bookcasePathKey.pre:
+        return 1;
+
+      case bookcasePathKey.peri:
+        return 2;
+
+      case bookcasePathKey.post:
+        return 3;
+
+      default:
+        return 0;
+    }
+  }, [navIndex]);
+
+  const contextDefaultValue: BookcaseNavigationContextType = {
+    navIndex: {
+      key: navIndex,
+      value: navUnderbarIndex,
+    },
+    changeNavIndex,
+  };
+
+  return (
+    <BookcaseNavigationContext.Provider value={contextDefaultValue}>{children}</BookcaseNavigationContext.Provider>
+  );
+};
